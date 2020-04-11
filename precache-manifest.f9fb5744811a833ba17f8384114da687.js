d3b1eb98ca08a324aff6d2890c609b272cd04e70
OmarBenitezL <isaibenitezlopez@gmail.com>
2020-04-11 16:02:01 -0500

    second

---
+++ precache-manifest.f9fb5744811a833ba17f8384114da687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5423ee22f671a5cdd2b542cf010cd732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1df8d9c2d12036da19f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1df8d9c2d12036da19f",
+    "url": "/static/js/main.0cf6a25b.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
